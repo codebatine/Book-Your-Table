fedea500ca2aab9ca17d779d52723816a9d22cc8
Sanjin Dumisic <codebatine@proton.me>
2024-04-07 02:44:46 +0200

    Dark Mode :crescent_moon:

--- therestaurant/src/pages/Layout.jsx
+++ therestaurant/src/pages/Layout.jsx
@@ -1,17 +1,20 @@
 import { Outlet } from "react-router";
 import "../App.scss";
 import { Navbar } from "../components/header/Navbar";
+import { DarkModeProvider } from "../context/DarkModeContext";
 
 export const Layout = () => {
   return (
-    <div>
-      <header>
-        <Navbar />
-      </header>
-      <main>
-        <Outlet />
-      </main>
-      <footer></footer>
-    </div>
+    <DarkModeProvider>
+      <div>
+        <header>
+          <Navbar />
+        </header>
+        <main>
+          <Outlet />
+        </main>
+        <footer></footer>
+      </div>
+    </DarkModeProvider>
   );
 };
