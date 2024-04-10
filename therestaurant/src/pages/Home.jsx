c0b11d0ba74fea92bb58385071fb3d590a383950
Sanjin Dumisic <codebatine@proton.me>
2024-04-10 08:17:06 +0200

    circle styled home :white_circle:

--- therestaurant/src/pages/Home.jsx
+++ therestaurant/src/pages/Home.jsx
@@ -8,6 +8,7 @@ export const Home = () => {
       <div className="bg bg3"></div>
       <div className="content">
         <h1>Book Your Table Today!</h1>
+        <p className="quote">Use Web3 to book an experience city&apos;s most exquisite restaurants</p>
         <NavLink to="/booking" className="cta-button">Book Now</NavLink>
       </div>
     </div>
