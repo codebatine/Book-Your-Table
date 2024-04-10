e67a87b0d3b4de033887f112c3bf29d5d3487f64
Sanjin Dumisic <codebatine@proton.me>
2024-04-10 08:49:25 +0200

    class fix :shell:

--- therestaurant/src/pages/Home.jsx
+++ therestaurant/src/pages/Home.jsx
@@ -8,7 +8,7 @@ export const Home = () => {
       <div className="bg bg3"></div>
       <div className="content">
         <h1>Book Your Table Today!</h1>
-        <p class="quote">Use <span class="web3">Web3</span> to book and experience the city's most exquisite restaurants.</p>
+        <p className="quote">Use <span className="web3">Web3</span> to book and experience the city&apos;s most exquisite restaurants.</p>
         <NavLink to="/booking" className="cta-button">Book Now</NavLink>
       </div>
     </div>
